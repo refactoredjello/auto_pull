7191929ac602238116d094aae16718b75eece3b2
refactoredjello <gil.mandler@gmail.com>
2019-04-07 19:31:55 -0400

    Puppeteer load site in - headless false

---
+++ index.js
@@ -0,0 +1,8 @@
+const puppeteer = require('puppeteer');
+
+
+(async () => {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.goto('https://app.periscopedata.com')
+})();
